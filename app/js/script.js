147058c0bde9ea46534bf319372805654afdc77b
Alessandro Torri <alessandro.torri@gmail.com>
2021-07-16 13:39:00 +0200

    DEBUG: click action on button
    DEBUG: click action on button

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,5 @@
-function changeClass() {
-  var x = document.getElementById("button").classList;
+function changeClass(ctrl) {
+  var x = document.getElementById(ctrl).classList;
   if (x.contains("testo-up")) {
     x.remove("testo-up");
     x.add("testo-down");
